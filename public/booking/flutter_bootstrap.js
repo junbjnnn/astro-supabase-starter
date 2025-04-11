d6bfd01e3c6036a860d6fadd985b28716455c70c
namdv-nals <121992494+namdv-nals@users.noreply.github.com>
2025-04-11 14:30:51 +0700

    fix: fix path

--- public/booking/flutter_bootstrap.js
+++ public/booking/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2200368362"
+    serviceWorkerVersion: "3786285374"
   }
 });
